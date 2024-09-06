bc27df90bb944e9f9255fb8b5455cbd3eb3b13f0
MRGUY10 <kamdemjordan58@gmail.com>
2024-09-06 13:35:06 +0200

    employee management system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { DetailsComponent } from './details/details.component';
+import {EditComponent} from "./edit/edit.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'edit/:id', component: EditComponent },
+
+];
